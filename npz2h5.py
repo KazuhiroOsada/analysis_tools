2f8ab3dbdd9d9ecad19027b16e5007a07a2829c1
Kazuhiro <167738270+KazuhiroOsada@users.noreply.github.com>
2026-02-24 13:45:47 +0900

    minor

--- npz2h5.py
+++ npz2h5.py
@@ -8,7 +8,7 @@ def npz2h5(npz_file, h5_file):
     with h5py.File(h5_file, 'w') as h5:
         for key in npz.files:
             arr = npz[key]
-            if arr.dtype.kind in {'U', 'S'}:  # Handle string arrays
+            if arr.dtype.kind in {'U', 'S'}:  # handle string arrays
                 dt = h5py.string_dtype(encoding='utf-8')
                 h5.create_dataset(key, data=arr.astype(object), dtype=dt)
             else:
